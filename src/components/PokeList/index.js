e905d2188260b41247a132805a75c2617000afb2
leireriel <leirericop@gmail.com>
2019-05-13 10:12:24 +0200

    Create public folder

--- src/components/PokeList/index.js
+++ src/components/PokeList/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Pokemon from '../Pokemon';
+import './PokeList.css';
 
 class PokeList extends React.Component {
   render() {
--- src/components/Pokemon/index.js
+++ src/components/Pokemon/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Pokemon.css';
 
 class Pokemon extends React.Component {
   render() {
